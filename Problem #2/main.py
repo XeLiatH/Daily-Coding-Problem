a0832cf37dca792f597daff4fb9f2fb744733c1f
matej <matej.beran96@gmail.com>
2019-06-03 20:51:18 +0200

    Coding problem #2 solution

---
+++ Problem #2/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf8 -*-
+
+"""Second problem solution"""
+
+
+def simple_approach(numbers: list):
+    """
+    The simplest, but dumbest solution
+    Complexity O(n^2) in the worst case
+
+    :param numbers: iterated array
+    :return: array of products
+    """
+
+    length = len(numbers)
+    array = [1] * length
+
+    for i in range(length):
+        for j in range(length):
+            if i != j:
+                array[i] *= numbers[j]
+
+    return array
+
+
+if __name__ == '__main__':
+    print(simple_approach([1, 2, 3, 4, 5]))
+    print(simple_approach([3, 2, 1]))
