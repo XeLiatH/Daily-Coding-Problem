7b11d76d23a564f0266fc93a28d0ac0a6ac615cc
Matěj Beran <matej.beran96@gmail.com>
2019-08-11 22:22:21 +0200

    Add more "test cases" in the main function.

--- Problem #04/four.py
+++ Problem #04/four.py
@@ -23,4 +23,6 @@ def find_missing_number(numbers: list):
 
 if __name__ == "__main__":
     print(2 == find_missing_number([3, 4, -1, 1]))
+    print(1 == find_missing_number([-2, -1, 10]))
     print(3 == find_missing_number([1, 2, 0]))
+    print(5 == find_missing_number([1, 2, 0, 4, 3, -1]))
