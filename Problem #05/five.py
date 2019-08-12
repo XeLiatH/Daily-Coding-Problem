1ae5773ee4730d6bf0147d589bcefc0ed460ecae
Matěj Beran <matej.beran96@gmail.com>
2019-08-12 09:21:29 +0200

    Add solution to the fifth coding problem.

---
+++ Problem #05/five.py
@@ -0,0 +1,24 @@
+# -*- coding: utf8 -*-
+
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(f):
+    def left(a, b):
+        return a
+    return f(left)
+
+
+def cdr(f):
+    def right(a, b):
+        return b
+    return f(right)
+
+
+if __name__ == "__main__":
+    print(car(cons(3, 4)))  # 3
+    print(cdr(cons(3, 4)))  # 4
