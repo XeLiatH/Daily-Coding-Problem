2f2c5e7a85ecd0442f8b7f4b0a318e4b1a781bd1
Matěj Beran <matej.beran96@gmail.com>
2020-02-24 15:28:15 +0100

    Improve solution 005.

--- 005/solution.py
+++ 005/solution.py
@@ -1,18 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+    > 005.solution
+    ~~~~~~~~~~~~~~
+
+"""
+
+
 def cons(a, b):
     def pair(f):
         return f(a, b)
+
     return pair
 
 
 def car(f):
     def left(a, b):
         return a
+
     return f(left)
 
 
 def cdr(f):
     def right(a, b):
         return b
+
     return f(right)
 
 
